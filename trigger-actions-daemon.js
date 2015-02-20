6aea886554a07fedbc9db6cb7368bdc8649d8598
inmysocks <inmysocks@gmail.com>
2015-02-19 21:29:10 -0700

    Added the listen filters, added the required stuff to prevent infinite loops. It should all work now.
    I may need to add some way to remove the 'listen_target' fields.

--- trigger-actions-daemon.js
+++ trigger-actions-daemon.js
@@ -30,6 +30,7 @@ Trigger Actions Daemon
 
 		// Reset the values when any of the tiddlers change.
 		$tw.wiki.addEventListener("change",function(changes) {
+			console.log("here");
 			//If the configuration changes do a full refresh, otherwise just refresh the changed expression.
 			if($tw.utils.hop(changes, CONFIGURATION_TIDDLER)) {
 				triggerActionsFull();
@@ -43,10 +44,32 @@ Trigger Actions Daemon
 					for (var j = 0; j < expressionTiddlerList.length; j++) {
 						var expressionTiddler = $tw.wiki.getTiddler(expressionTiddlerList[j]);
 						if(expressionTiddler) {
+							if(expressionTiddler.getFieldString("listen_target") !== "true") {
+								var fields = {expressionTiddler: expressionTiddler};
+								fields["listen_target"] = "true";
+								$tw.wiki.addTiddler(new $tw.Tiddler(expressionTiddler,fields,undefined));
+							}
 							// If the expression tiddler has changed, update its output.
 							if($tw.utils.hop(changes,expressionTiddlerList[j])) {
 								evaluateExpression(expressionTiddler);
-							} 
+							} else {
+								// If any of the tiddlers listed in the listenFilter change update the output.
+								var listenFilter = expressionTiddler.getFieldString("listen_filter");
+								var listenTiddlerList = $tw.wiki.filterTiddlers(listenFilter);
+								if(listenTiddlerList.length !== 0) {
+									for (var p = 0; p < listenTiddlerList.length; p++) {
+										var currentListenTiddler = $tw.wiki.getTiddler(listenTiddlerList[p])
+										if(currentListenTiddler.getFieldString("listen_target") !== "true") {
+											var fields = {currentListenTiddler: currentListenTiddler};
+											fields["listen_target"] = "true";
+											$tw.wiki.addTiddler(new $tw.Tiddler(currentListenTiddler,fields,undefined));
+										}
+										if($tw.utils.hop(changes,listenTiddlerList[p])) {
+											evaluateExpression(expressionTiddler);
+										}
+									}
+								}
+							}
 						}
 					}
 				}
@@ -54,12 +77,12 @@ Trigger Actions Daemon
 		});
 	};
 
-	// This returns the content of all fields execpt: title, text, modified, created, creator, tags, evaluate
+	// This returns the content of all fields execpt: title, text, modified, created, creator, tags, evaluate, listen_filter, action_filter
 	function getTiddlerFields(tiddler) {
 		var results = [];		
 		if(tiddler) {
 			for(var fieldName in tiddler.fields) {
-				if(fieldName != "title" && fieldName != "text" && fieldName != "modified" && fieldName != "created" && fieldName != "creator" && fieldName !="tags" && fieldName != "evaluate") {
+				if(fieldName != "title" && fieldName != "text" && fieldName != "modified" && fieldName != "created" && fieldName != "creator" && fieldName !="tags" && fieldName != "evaluate" && fieldName != "listen_filter" && fieldName != "action_filter") {
 					$tw.utils.pushTop(results,fieldName);
 				}
 			}
@@ -83,7 +106,7 @@ Trigger Actions Daemon
 	// This should be simple, it just takes each expression tiddler, evaluates its filter and then performs the actions on each tiddler returned by the filter.
 	// The probelm is I don't know how to evaluate a wikitext string from javascript.
 	function evaluateExpression(expressionTiddler) {
-		var expressionFilter = expressionTiddler.getFieldString("text"); // This is in a specific field in the expressionTiddler.
+		var expressionFilter = expressionTiddler.getFieldString("action_filter"); // This is in a specific field in the expressionTiddler.
 		var fieldList = getTiddlerFields(expressionTiddler); // This lists all of the action fields in the expression tiddler
 		var actionList = getActionList(expressionTiddler, fieldList); // This lists the contents of all other fields of the expressionTiddler;
 
@@ -92,14 +115,17 @@ Trigger Actions Daemon
 		for(var i=0; i<actionTiddlers.length; i++) {
 			var currentActionTiddler = $tw.wiki.getTiddler(actionTiddlers[i]); // How do we set currentActionTiddler as the current tiddler?
 			if(currentActionTiddler) {
-				for(var l=0; l<actionList.length; l++) {
-					var actionItem = actionList[l];
-					var parsed = $tw.wiki.parseText("text/vnd.tiddlywiki", actionList[l], {});
-					var widgets = $tw.wiki.makeWidget(parsed, {});
-					var container = $tw.fakeDocument.createElement("div");
-					widgets.setVariable("currentTiddler", currentActionTiddler.getFieldString("title"));
-					widgets.render(container, null);
-					widgets.children[0].invokeActions({});
+				// Only act if changing the current action tiddler will not trigger another set of actions.
+				if(currentActionTiddler.getFieldString("listen_target") !== "true") {
+					for(var l=0; l<actionList.length; l++) {
+						var actionItem = actionList[l];
+						var parsed = $tw.wiki.parseText("text/vnd.tiddlywiki", actionList[l], {});
+						var widgets = $tw.wiki.makeWidget(parsed, {});
+						var container = $tw.fakeDocument.createElement("div");
+						widgets.setVariable("currentTiddler", currentActionTiddler.getFieldString("title"));
+						widgets.render(container, null);
+						widgets.children[0].invokeActions({});
+					}
 				}
 			}
 		}
